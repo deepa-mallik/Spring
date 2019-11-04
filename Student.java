7cc38c44e0cc7caa81246c35d808eb769d702b73
deepa-mallik <57281904+deepa-mallik@users.noreply.github.com>
2019-11-04 16:08:37 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,26 @@
+public class Student 
+{  
+private int empid;
+private String name,address;
+
+
+public void setEmpid(int empid) {            
+	this.empid = empid;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public void displayInfo(){  
+    System.out.println("the empid is "+empid+"\n"+"name is "+name+"\n"+"the address is "
++address );  
+}  
+}  
+
+
+
